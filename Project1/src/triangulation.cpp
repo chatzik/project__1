2b19d34a51f6282145f609f661aec7a18027a90f
t225 <thch116@gmail.com>
2024-10-26 00:55:35 +0300

    V4 - 1st method

--- Project1/src/triangulation.cpp
+++ Project1/src/triangulation.cpp
@@ -1,13 +1,16 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/mark_domain_in_triangulation.h>
 #include <CGAL/Delaunay_mesher_2.h>
 #include "triangulation.h"
+#include <CGAL/Polygon_2.h>
 #include <CGAL/draw_triangulation_2.h>
-#include <cmath> // Χρήση της βιβλιοθήκης math για το εσωτερικό γινόμενο
+#include <unordered_map>
+#include <cmath>
 
 //////////////////////////////////////////////////////////
 
-// Ορισμός του Kernel
+// kernel
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Triangulation_vertex_base_2<K> Vb;
 typedef CGAL::Constrained_triangulation_face_base_2<K> Fb;
@@ -15,8 +18,10 @@ typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
 typedef CGAL::Exact_predicates_tag Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<K, Tds, Itag> CDT;
 typedef CDT::Point Point;
+typedef CDT::Face_handle Face_handle;
 using namespace std;
 
+///////////////////////////////////////////////////////////
 // υπολογισμός εσωτερικού γινομένου δύο διανυσμάτων (χρησιμοποιεί την βιβλιοθήκη math)
 double dotProduct(const Point &a, const Point &b, const Point &c)
 {
